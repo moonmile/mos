1d96c2ca76c7565679455dbc26e39e49f5208a2f
Tomoaki Masuda <masuda@moonmile.net>
2023-06-21 00:40:16 +0900

    add pinia

--- temp/vue-master2-pinia/src/main.ts
+++ temp/vue-master2-pinia/src/main.ts
@@ -4,14 +4,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import { store } from './stores/store'
-
 import './assets/main.css'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(createPinia()) // pinia の利用
 app.use(router)
-app.use(store) // vuex の利用
 
 app.mount('#app')
